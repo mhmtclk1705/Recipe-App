7c7545c7f603a19b73e0dbc8824a9dbeb495b4c5
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-05 16:28:30 +0300

    edit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React,{useEffect, useState} from 'react'
+import React,{useState} from 'react'
 import RecipeCard from './RecipeCard'
 import {BgImg, FoodContainer, FormContainer} from "./style"
 import homeSvg from '../../assets/home.svg'
