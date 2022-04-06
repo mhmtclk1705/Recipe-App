81013e2903f8f2ee54133b3ed0b98c021735d3a4
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-06 21:52:02 +0300

    edit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <NavbarContainer>
         <div className="navbar-left">
-            <NavLink to="/"> <i> {"<CLARUSWAY>"} </i> <span>RECIPE</span></NavLink></div>
+            <NavLink to="/home"> <i> {"<CLARUSWAY>"} </i> <span>RECIPE</span></NavLink></div>
         <div className="navbar-right">
             <NavLink to='/about'>ABOUT</NavLink>
             <a target="_blank" href="https://github.com/mhmtclk1705">GITHUB</a>
